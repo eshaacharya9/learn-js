1b1005e8ca568ab6c3347d27d4077c20cd942bf3
esha <acharya.e@northeastern.edu>
2024-02-01 14:55:36 -0500

    edited jan 30

--- quiz/scripts/logger.js
+++ quiz/scripts/logger.js
@@ -1,1 +1,16 @@
 // Define a JavaScript function called logMsg() that can be used to log an error message for any object that contains the property errMsg. 
+function logMsg(){
+    console.log('erro msg:${this.errmgs}');
+
+}
+
+const o1 ={
+    errMsg:'01'
+}
+
+const o2 ={
+    errMsg:'02'
+}
+
+errMsg.call(o1);
+errMsg.call(o2);
